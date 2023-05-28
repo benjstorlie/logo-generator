af1117aa094cd276acb0ea7ec3cf285ff42fcac6
bjs <127002178+benstorlie@users.noreply.github.com>
2023-05-28 16:57:32 -0700

    starting inquierer script

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 
 const {Shape, Triangle, Circle, Square} = require('./lib/shapes.js')
 const Logo = require('./lib/logo.js')
+
+// Import Inquirer for command-line interaction
+const inquirer = require('inquirer');
+
+
