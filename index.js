4709726097f84f7a10baa5d859abb7055a8065f9
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2023-05-30 15:41:08 -0700

    It's all mostly done

--- index.js
+++ index.js
@@ -10,28 +10,54 @@ const inquirer = require('inquirer');
 
 
 
-function init() {
+function buildSVG() {
 
-  inquirer.prompt(questions).then((answers) => {
+  console.log('buildSVG')
+
+  inquirer.prompt(questions,{cont: false}).then((answers) => {
+    console.log('ans:'+answers)
+    const { shape,
+      text,
+      textColorList,
+      textColorInput,
+      fontSizeList,
+      fillColorList,
+      fillColorInput
+    } = answers;
     let logo = new Logo({
-      textColor: answersColor(answers.textColorList,answers.textColorInput),
-      text: answers.text,
-    })
-    if (answers.shape !== 'no shape') {
-      logo.setFillColor(answersColor(answers.fillColorList, answers.fillColorInput))
-      logo.setShape(answers.shape);
+      textColor: answersColor(textColorList,textColorInput),
+      text: text,
+    });
+    fontSizeList.pop();
+    logo.setTextSize({value: Number(fontSizeList)})
+    if (shape !== 'no shape') {
+      logo.setFillColor(answersColor(fillColorList, fillColorInput))
+      logo.setShape(shape);
     } 
 
     try {
-      writeToFile(titleFileName(answers.text),logo.render(),'logos')
+      writeToFile(titleFileName(text),logo.render(),'logos')
     } catch(err) {
       console.log(err);
       console.log(logo.render()) // At least you don't lose your data and have to do it all over again if it doesn't save correctly.
     }
+    })
 
-  });
+    answers.continue = true
+    inquirer.prompt(questions,answers)
+    
+}
 
-  
+
+function edit(logo, edit) {
+  edit.continue = true;
+  inquirer.prompt(questions,edit).then((answers) => {
+
+  })
+}
+
+function init() {
+  buildSVG();
 }
 
 init()
